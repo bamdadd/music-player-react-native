0c819355654918bbbd1dd45cac86dca0cc1b4e9a
Bamdad Dashtban <bamdadd@gmail.com>
2018-06-01 14:10:16 +0100

    Currently playing reducers

--- src/reducer.js
+++ src/reducer.js
@@ -1,39 +1,74 @@
-export const rootReducer = (state = initialState, action) => {
+import {combineReducers} from 'redux';
+
+const initialGenres = ["Alternative Rock", "Ambient", "Classical", "Country", "EDM",
+                       "Dancehall", "Deep House", "Disco", "Drum & Bass", "Dubstep", "Electronic",
+                       "Folk", "Singer-Songwriter", "Rap", "House", "Indie", "Jazz & Blues",
+                       "Latin", "Metal", "Piano", "Pop", "R&B & Soul", "Reggae", "Reggaeton",
+                       "Rock", "Soundtrack", "Techno", "Trance", "Trap", "Triphop"].map(
+                           (name, i) => ({
+                               id: i,
+                               name
+                           }));
+
+const songs = (state = {}, action) => {
     switch (action.type) {
-        case 'PLAYING_GENRE':
-            return Object.assign({}, state,
-                                 {
-                                     currentlyPlaying: action.genre
-                                 });
         case 'FOUND_SONGS':
             const { songs, genre } = action;
-            return Object.assign({}, state,
+            return Object.assign({}, state.songs,
                                  {
-                                     songs: Object.assign({}, state.songs,
-                                                          {
-                                                              [genre.id]: songs
-                                                          })
+                                     [genre.id]: songs
                                  })
         default:
             return state;
     }
 };
 
+const genres = (state = initialGenres, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
 
-export const initialState = {
-    genres: ["Alternative Rock", "Ambient", "Classical", "Country", "EDM",
-             "Dancehall", "Deep House", "Disco", "Drum & Bass", "Dubstep", "Electronic",
-             "Folk", "Singer-Songwriter", "Rap", "House", "Indie", "Jazz & Blues",
-             "Latin", "Metal", "Piano", "Pop", "R&B & Soul", "Reggae", "Reggaeton",
-             "Rock", "Soundtrack", "Techno", "Trance", "Trap", "Triphop"].map(
-                 (name, i) => ({
-                     id: i,
-                     name
-                 })
-             ),
-    currentlyPlaying: {
-        id: null,
-        name: null
-    },
-    songs: {}
-};
+
+const initialCurrentlyPlaying = {
+    genre: {},
+    songIndex: -1,
+    paused: false
+}
+
+const currentlyPlaying = (state = initialCurrentlyPlaying, action) => {
+    switch (action.type) {
+        case 'PLAYING_GENRE':
+            return Object.assign({}, state,
+                                 {
+                                     genre: action.genre,
+                                     paused: false,
+                                     currentTime: 0
+                                 });
+        case 'SET_CURRENT_SONG':
+            return Object.assign({}, state,
+                                 {
+                                     songIndex: action.index
+                                 });
+        case 'UPDATE_PAUSED':
+            return Object.assign({}, state,
+                                 {
+                                     paused: action.paused
+                                 });
+        case 'SET_PLAY_TIME':
+            return Object.assign({}, state,
+                                 {
+                                     currentTime: action.currentTime
+                                 });
+
+        default:
+            return state;
+    }
+}
+
+export const rootReducer = combineReducers({
+    currentlyPlaying,
+    songs,
+    genres
+});
